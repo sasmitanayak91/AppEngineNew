b763134ad4467d6e74dcdda81184dbaab53dc797
sasmita_nayak1 <sasmita.n3@gmail.com>
2022-07-01 18:13:09 +0000

    modified main.py to test autobuild4

--- hello-world/main.py
+++ hello-world/main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello,TestAgain')
+        self.response.write('Hello,Test')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
