6863da874bf4c76d48f7bf6462e468ff5714e8bd
sasmita_nayak1 <sasmita.n3@gmail.com>
2022-07-01 17:44:14 +0000

    Update main.py to test autodeployment

--- hello-world/main.py
+++ hello-world/main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Goodbye, Moon!')
+        self.response.write('Lets Test the autodeployment')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
